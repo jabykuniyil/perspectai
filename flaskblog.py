3f102f5be9ab5f842e277cb62817652166b9c988
jaby <mohdjabiran112@gmail.com>
2021-06-07 14:34:34 +0530

    task completed

--- flaskblog.py
+++ flaskblog.py
@@ -2,15 +2,21 @@ from functools import wraps
 from flask import Flask, request, jsonify, make_response
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
-import datetime, jwt, secrets
+import datetime, jwt, secrets, os
+from werkzeug.utils import secure_filename
+
 
 app = Flask(__name__)
+#generating the secret_key.
 generated_key = secrets.token_urlsafe(50)
 app.config['SECRET_KEY'] = generated_key
+#setting up the database configuration
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:jaby@localhost/perspectai'
 db = SQLAlchemy(app)
 
+#creating table for the users.
 class Account(db.Model):
+    __tablename__ = 'user'
     username = db.Column(db.String(20), primary_key=True, nullable=False)
     full_name = db.Column(db.String(50), nullable=False)
     password_hash = db.Column(db.String(200), nullable=False)
@@ -20,15 +26,30 @@ class Account(db.Model):
         self.full_name = full_name
         self.password_hash = password_hash
 
+#creating table for the blog.
 class Blog(db.Model):
+    __tablename__ = 'blog'
     id = db.Column(db.Integer, primary_key=True)
     post = db.Column(db.String(200))
-    image = db.Column(db.LargeBinary)
+    archive = db.Column(db.Boolean(), default=False, nullable=False)
 
-    
-    def __init__(self, post):
+    def __init__(self, id, post):
+        self.id = id
         self.post = post
         
+#saving the files to particular folder.
+UPLOAD_FOLDER = 'static/uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+
+#allowing extensions.
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+#validating the file is valid or not.
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+        
+#creating account for the user and make hashing the password.
 @app.route('/', methods=['POST'])
 def create_account():
     user_data = request.get_json()
@@ -39,6 +60,8 @@ def create_account():
         'message' : 'New User created successfully'
     })
     
+    
+#to get the all users in the database.
 @app.route('/users', methods=['GET'])
 def all_users():
     users = Account.query.all()
@@ -50,6 +73,7 @@ def all_users():
         output.append(user)
     return jsonify({'users' : output})
 
+#decorator used to call this function for every function to chek wheather the token is available or not.
 def check_for_token(func):
     @wraps(func)
     def wrapped(*args, **kwargs):
@@ -66,6 +90,7 @@ def check_for_token(func):
         return func(current_user, *args, **kwargs)
     return wrapped
 
+#login function and generating token for each logged users with expiring time.
 @app.route('/login', methods=['GET'])
 def login():
     auth = request.authorization
@@ -85,25 +110,56 @@ def login():
     
     return make_response('Could not verify', 401, {'WWW-Authenticate' : 'Basic realm="Login required!"'})
     
+    
+""" GET method is used to get the all blogs in the database except archived. POST method is used to create a new blog.
+"""
 @app.route('/blogs', methods=['GET', 'POST'])
+#callling function to check the token for current user is available or not
 @check_for_token
 def home(current_user):
     if request.method == 'POST':
-        blog_data = request.get_json()
-        blog = Blog(post=blog_data['post'])
-        db.session.add(blog)
-        db.session.commit()
-        return jsonify(blog_data)
-    all_blogs = Blog.query.all()
+        if 'files[]' not in request.files:
+            blog_data = request.get_json()
+            blog = Blog(post=blog_data['post'])
+            db.session.add(blog)
+            db.session.commit()
+            return jsonify(blog_data)
+        files = request.files.getlist('files[]')
+        errors = {}
+        success = False
+        for file in files:      
+            if file and allowed_file(file.filename):
+                filename = secure_filename(file.filename)
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                success = True
+            else:
+                errors[file.filename] = 'File type is not allowed'
+        if success and errors:
+            errors['message'] = 'File(s) successfully uploaded'
+            resp = jsonify(errors)
+            resp.status_code = 500
+            return resp
+        if success:
+            resp = jsonify({'message' : 'Files successfully uploaded'})
+            resp.status_code = 201
+            return resp
+        else:
+            resp = jsonify(errors)
+            resp.status_code = 500
+            return resp
+    blogs = Blog.query.filter_by(archive=False)
     output = []
-    for blog in all_blogs:
+    for blog in blogs:
         curr_blog = {}
         curr_blog['post'] = blog.post
         output.append(curr_blog)
     return jsonify(output)
 
 
-@app.route('/blog/<int:id>', methods=['GET', 'PUT', 'DELETE'])
+""" GET method is for to get the particular post with respect to the id in the url. POST method is to archive the particular post. PUT method is to update the particular post. DELETE method is to delete the particular post.
+"""
+@app.route('/blog/<int:id>', methods=['GET', 'PUT', 'DELETE', 'POST'])
+#callling function to check the token for current user is available or not
 @check_for_token
 def single_blog(current_user, id):
     blog = db.session.query(Blog).filter_by(id=id).first()
@@ -122,6 +178,13 @@ def single_blog(current_user, id):
             Blog.query.filter_by(id=id).delete()
             db.session.commit()
             return "Your blog has been deleted", 201
+        elif request.method == 'POST':
+            curr_blog['id'] = request.json['id']
+            curr_blog['archive'] = True
+            curr_blog['post'] = blog.post
+            curr_blog['message'] = 'This data has been archived'
+            db.session.commit()
+            return curr_blog
         else:
             return jsonify({'message' : 'method doesn"t support'})
     return "url doesn't exists", 404
